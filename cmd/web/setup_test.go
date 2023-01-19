288cdd523b13c7c66affa45a96bd14f53e22e5d4
Prosper Atu <atu.prosper@gmail.com>
2023-01-19 05:16:24 +0100

    Created setup test file

---
+++ cmd/web/setup_test.go
@@ -0,0 +1,22 @@
+package main
+
+// This file will run before any test file will run
+
+import (
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestMain(mainTest *testing.M) {
+
+	// Before the test starts running, do something inside the `Run()` function, then exit
+	os.Exit(mainTest.Run())
+}
+
+// `myHandler` holds objects that will satisfy http handler interface
+type myHandler struct{}
+
+func (handlerObject *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
