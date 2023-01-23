a11e40c7b0881a50506fb992c0e4e8e2f691eb1e
Prosper Atu <atu.prosper@gmail.com>
2023-01-23 22:35:40 +0100

    Wrote the Post handler for handlers test

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -19,7 +19,7 @@ var theTests = []struct {
 	params             []postData
 	expectedStatusCode int
 }{
-	{"home", "/", "GET", []postData{}, http.StatusOK},	
+	{"home", "/", "GET", []postData{}, http.StatusOK},
 	{"about", "/about", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
 	{"alpine", "/alpine", "GET", []postData{}, http.StatusOK},
@@ -27,6 +27,20 @@ var theTests = []struct {
 	{"reservation", "/reservation", "GET", []postData{}, http.StatusOK},
 	{"make-reservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
 	{"reservation-summary", "/reservation-summary", "GET", []postData{}, http.StatusOK},
+	{"post-reservation", "/reservation", "POST", []postData{
+		{key: "start", value: "2023-01-01"},
+		{key: "end", value: "2023-01-02"},
+	}, http.StatusOK},
+	{"post-reservation-json", "/reservation-json", "POST", []postData{
+		{key: "start", value: "2023-01-01"},
+		{key: "end", value: "2023-01-02"},
+	}, http.StatusOK},
+	{"make-reservation", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "Prosper"},
+		{key: "last_name", value: "Atu"},
+		{key: "email", value: "me@email.com"},
+		{key: "phone", value: "555-555-5555"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(testPointer *testing.T) {
@@ -35,31 +49,31 @@ func TestHandlers(testPointer *testing.T) {
 	testServer := httptest.NewTLSServer(routes)
 	defer testServer.Close()
 
-	for _, e := range theTests {
-		if e.method == "GET" {
-			resp, err := testServer.Client().Get(testServer.URL + e.url)
+	for _, element := range theTests {
+		if element.method == "GET" {
+			resp, err := testServer.Client().Get(testServer.URL + element.url)
 			if err != nil {
 				testPointer.Log(err)
 				testPointer.Fatal(err)
 			}
 
-			if resp.StatusCode != e.expectedStatusCode {
-				testPointer.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			if resp.StatusCode != element.expectedStatusCode {
+				testPointer.Errorf("for %s expected %d but got %d", element.name, element.expectedStatusCode, resp.StatusCode)
 			}
 		} else {
 			values := url.Values{}
-			for _, x := range e.params {
-				values.Add(x.key, x.value)
+			for _, item := range element.params {
+				values.Add(item.key, item.value)
 			}
 
-			resp, err := testServer.Client().PostForm(testServer.URL+e.url, values)
+			resp, err := testServer.Client().PostForm(testServer.URL+element.url, values)
 			if err != nil {
 				testPointer.Log(err)
 				testPointer.Fatal(err)
 			}
 
-			if resp.StatusCode != e.expectedStatusCode {
-				testPointer.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			if resp.StatusCode != element.expectedStatusCode {
+				testPointer.Errorf("for %s expected %d but got %d", element.name, element.expectedStatusCode, resp.StatusCode)
 			}
 		}
 	}
