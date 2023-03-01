10589a87d672f91c24e543a185ca9a4f6c4f4253
Prosper Atu <atu.prosper@gmail.com>
2023-03-01 21:17:08 +0100

    Added SearchAvailabilityByDatesByRoomID to the repositoy

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,14 @@
 package repository
 
-import "github.com/atuprosper/booking-project/internal/models"
+import (
+	"time"
+
+	"github.com/atuprosper/booking-project/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(res models.RoomRestriction) error
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
