2570f24ca09dfa26b987a8d0757151b77803cd1e
Prosper Atu <atu.prosper@gmail.com>
2023-03-03 22:10:22 +0100

    Added GetRoomByID type in the repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -8,8 +8,10 @@ import (
 
 type DatabaseRepo interface {
 	AllUsers() bool
+
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(res models.RoomRestriction) error
 	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
+	GetRoomByID(id int) (models.Room, error)
 }
