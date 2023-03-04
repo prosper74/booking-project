20037f95555d96e4da52576eb299f7f665a61bbe
Prosper Atu <atu.prosper@gmail.com>
2023-03-04 16:33:08 +0100

    added GetUserByID to the repositry types

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -14,4 +14,6 @@ type DatabaseRepo interface {
 	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 	GetRoomByID(id int) (models.Room, error)
+
+	GetUserByID(id int) (models.User, error)
 }
