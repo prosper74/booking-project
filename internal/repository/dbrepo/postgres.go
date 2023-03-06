617f5ba2743cb73dca7f0ca786dba4b9551821fc
Prosper Atu <atu.prosper@gmail.com>
2023-03-06 16:15:40 +0100

    Created authentication function

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -2,9 +2,11 @@ package dbrepo
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/atuprosper/booking-project/internal/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (repo *postgresDBRepo) AllUsers() bool {
@@ -191,4 +193,28 @@ func (m *postgresDBRepo) UpdateUser(u models.User) error {
 	}
 
 	return nil
+}
+
+// Authenticate authenticates a user
+func (m *postgresDBRepo) Authenticate(email, testPassword string) (int, string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var id int
+	var hashedPassword string
+
+	row := m.DB.QueryRowContext(ctx, "select id, password from users where email = $1", email)
+	err := row.Scan(&id, &hashedPassword)
+	if err != nil {
+		return id, "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(testPassword))
+	if err == bcrypt.ErrMismatchedHashAndPassword {
+		return 0, "", errors.New("incorrect password")
+	} else if err != nil {
+		return 0, "", err
+	}
+
+	return id, hashedPassword, nil
 }
