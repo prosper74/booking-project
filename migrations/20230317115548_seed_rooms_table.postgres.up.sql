058b933efcc30860abd4c469f1bb4a11849fb37e
Prosper Atu <atu.prosper@gmail.com>
2023-03-17 13:00:12 +0100

    set up the up migration for seed room tables

---
+++ migrations/20230317115548_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Generals Suit','2023-02-26 00:00:00.000','2023-02-25 00:00:00.000'),
+	 ('Luxery One','2023-03-10 00:00:00.000','2023-03-10 00:00:00.000');
