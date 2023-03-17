e111372244463aa0388dfd44f26ae40949c95284
Prosper Atu <atu.prosper@gmail.com>
2023-03-17 13:07:27 +0100

    Setup the up migration for seeding restrictions table

---
+++ migrations/20230317120259_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2023-02-25 00:00:00.000','2023-02-25 00:00:00.000'),
+	 ('Owners Block','2023-02-05 00:00:00.000','2023-02-05 00:00:00.000');
