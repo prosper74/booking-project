70c075efa3951195d6e186f7369d9b3900bc9f96
Prosper Atu <atu.prosper@gmail.com>
2023-03-18 17:27:35 +0100

    Removed unused types in the repository.go

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -14,13 +14,4 @@ type DatabaseRepo interface {
 	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 	GetRoomByID(id int) (models.Room, error)
-
-	GetUserByID(id int) (models.User, error)
-	UpdateUser(u models.User) error
-	Authenticate(email, testPassword string) (int, string, error)
-
-	AllReservations() ([]models.Reservation, error)
-	AllNewReservations() ([]models.Reservation, error)
-	GetReservationByID(id int) (models.Reservation, error)
-	UpdateReservation(u models.Reservation) error
 }
