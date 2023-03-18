b54f69fb832f1d6be4d6df81f41bcd0588cbaafb
Prosper Atu <atu.prosper@gmail.com>
2023-03-18 17:33:11 +0100

    Removed db queries from the test repo

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -1,7 +1,6 @@
 package dbrepo
 
 import (
-	"context"
 	"time"
 
 	"github.com/atuprosper/booking-project/internal/models"
@@ -13,129 +12,29 @@ func (repo *testDBRepo) AllUsers() bool {
 
 // Inserts a reservation into the database
 func (repo *testDBRepo) InsertReservation(res models.Reservation) (int, error) {
-	// Close this transaction if unable to run this statement within 3 seconds
-	context, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	var newID int
-
-	insertStatement := `insert into reservations (first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
-
-	err := repo.DB.QueryRowContext(context, insertStatement, res.FirstName, res.LastName, res.Email, res.Phone, res.StartDate, res.EndDate, res.RoomID, time.Now(), time.Now()).Scan(&newID)
-
-	if err != nil {
-		return 0, err
-	}
-
-	return newID, nil
+	return 1, nil
 }
 
 // InsertRoomRestriction inserts a room restriction into the database
 func (repo *testDBRepo) InsertRoomRestriction(res models.RoomRestriction) error {
-	context, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	insertStatement := `insert into room_restrictions (start_date, end_date, room_id, reservation_id, created_at, updated_at, restriction_id) values($1, $2, $3, $4, $5, $6, $7)`
-
-	_, err := repo.DB.ExecContext(context, insertStatement, res.StartDate, res.EndDate, res.RoomID, res.ReservationID, time.Now(), time.Now(), res.RestrictionID)
-
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
 // SearchAvailabilityByDatesByRoomID returns true if availability exists for roomID, and false if no availability
 func (repo *testDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
-	context, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	var numRows int
-
-	query := `
-		select
-			count(id)
-		from
-			room_restrictions
-		where
-			room_id = $1
-			and $2 < end_date and $3 > start_date;`
-
-	row := repo.DB.QueryRowContext(context, query, roomID, start, end)
-	err := row.Scan(&numRows)
-	if err != nil {
-		return false, err
-	}
-
-	if numRows == 0 {
-		return true, nil
-	}
 	return false, nil
 }
 
 // SearchAvailabilityForAllRooms returns a slice of available rooms, if any, for given date range
 func (repo *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
-	context, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
 	var rooms []models.Room
-
-	query := `
-		select
-			r.id, r.room_name
-		from
-			rooms r
-		where r.id not in 
-		(select room_id from room_restrictions rr where $1 < rr.end_date and $2 > rr.start_date);
-	`
-
-	rows, err := repo.DB.QueryContext(context, query, start, end)
-	if err != nil {
-		return rooms, err
-	}
-
-	for rows.Next() {
-		var room models.Room
-		err := rows.Scan(
-			&room.ID,
-			&room.RoomName,
-		)
-		if err != nil {
-			return rooms, err
-		}
-		rooms = append(rooms, room)
-	}
-
-	if err = rows.Err(); err != nil {
-		return rooms, err
-	}
 
 	return rooms, nil
 }
 
 // GetRoomByID gets a room by id
 func (repo *testDBRepo) GetRoomByID(id int) (models.Room, error) {
-	context, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
 	var room models.Room
-
-	query := `
-		select id, room_name, created_at, updated_at from rooms where id = $1
-	`
-
-	row := repo.DB.QueryRowContext(context, query, id)
-	err := row.Scan(
-		&room.ID,
-		&room.RoomName,
-		&room.CreatedAt,
-		&room.UpdatedAt,
-	)
-
-	if err != nil {
-		return room, err
-	}
 
 	return room, nil
 }
