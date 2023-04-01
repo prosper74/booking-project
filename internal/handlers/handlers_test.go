fd5cd4938663b4d37765d375ad484e31e0a6afe5
Prosper Atu <atu.prosper@gmail.com>
2023-04-01 14:30:51 +0100

    Created getContext function for handles test

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"context"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -23,22 +25,6 @@ var theTests = []struct {
 	{"alpine", "/alpine", "GET", http.StatusOK},
 	{"generals", "/generals", "GET", http.StatusOK},
 	{"reservation", "/reservation", "GET", http.StatusOK},
-	// {"make-reservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
-	// {"reservation-summary", "/reservation-summary", "GET", []postData{}, http.StatusOK},
-	// {"post-reservation", "/reservation", "POST", []postData{
-	// 	{key: "start", value: "2023-01-01"},
-	// 	{key: "end", value: "2023-01-02"},
-	// }, http.StatusOK},
-	// {"post-reservation-json", "/reservation-json", "POST", []postData{
-	// 	{key: "start", value: "2023-01-01"},
-	// 	{key: "end", value: "2023-01-02"},
-	// }, http.StatusOK},
-	// {"make-reservation", "/make-reservation", "POST", []postData{
-	// 	{key: "first_name", value: "Prosper"},
-	// 	{key: "last_name", value: "Atu"},
-	// 	{key: "email", value: "me@email.com"},
-	// 	{key: "phone", value: "555-555-5555"},
-	// }, http.StatusOK},
 }
 
 func TestHandlers(testPointer *testing.T) {
@@ -48,15 +34,22 @@ func TestHandlers(testPointer *testing.T) {
 	defer testServer.Close()
 
 	for _, element := range theTests {
-		resp, err := testServer.Client().Get(testServer.URL + element.url)
+		response, err := testServer.Client().Get(testServer.URL + element.url)
 		if err != nil {
 			testPointer.Log(err)
 			testPointer.Fatal(err)
 		}
 
-		if resp.StatusCode != element.expectedStatusCode {
-			testPointer.Errorf("for %s expected %d but got %d", element.name, element.expectedStatusCode, resp.StatusCode)
+		if response.StatusCode != element.expectedStatusCode {
+			testPointer.Errorf("for %s expected %d but got %d", element.name, element.expectedStatusCode, response.StatusCode)
 		}
-
 	}
+}
+
+func getContext(req *http.Request) context.Context {
+	ctx, err := session.Load(req.Context(), req.Header.Get("X-Session"))
+	if err != nil {
+		log.Println(err)
+	}
+	return ctx
 }
