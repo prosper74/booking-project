fc6d3c81e4518a7759738b42785a490e7d039ee9
Prosper Atu <atu.prosper@gmail.com>
2023-04-06 21:47:27 +0100

    Added MailChannel to site wide config

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/alexedwards/scs/v2"
+	"github.com/atuprosper/booking-project/internal/models"
 )
 
 type AppConfig struct {
@@ -14,4 +15,5 @@ type AppConfig struct {
 	ErrorLog      *log.Logger
 	InProduction  bool
 	Session       *scs.SessionManager
+	MailChannel   chan models.MailData
 }
