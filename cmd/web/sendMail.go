971c5cdd94ac4ce4deb20cad4d6cfe0868164707
Prosper Atu <atu.prosper@gmail.com>
2023-04-06 22:11:16 +0100

    Created the mail listener function

---
+++ cmd/web/sendMail.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/atuprosper/booking-project/internal/models"
+	mail "github.com/xhit/go-simple-mail/v2"
+)
+
+func listenForMail() {
+	go func() {
+		for {
+			message := <-app.MailChannel
+			sendMessage(message)
+		}
+	}()
+}
+
+func sendMessage(m models.MailData) {
+	server := mail.NewSMTPClient()
+	server.Host = "localhost"
+	server.Port = 1025
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	client, err := server.Connect()
+	if err != nil {
+		errorLog.Println(err)
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(m.From).AddTo(m.To).SetSubject(m.Subject)
+	email.SetBody(mail.TextHTML, "Hello, <strong>Prosper</strong>")
+
+	err = email.Send(client)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Email Sent")
+	}
+
+}
