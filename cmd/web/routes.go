ebd5a71b1344ac81a18c45b931833798d5eb8d25
Prosper Atu <atu.prosper@gmail.com>
2023-04-16 13:03:48 +0100

    Created route for AdminSingleReservation

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -48,6 +48,8 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Get("/new-reservations", handlers.Repo.AdminNewReservations)
 		mux.Get("/all-reservations", handlers.Repo.AdminAllReservations)
 		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
+
+		mux.Get("/reservations/{src}/{id}", handlers.Repo.AdminSingleReservation)
 	})
 
 	return mux
