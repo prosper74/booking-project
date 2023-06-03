76f00b860fb56af2306427832997f0066372cc93
Prosper Atu <atu.prosper@gmail.com>
2023-06-03 02:07:08 +0100

    Removed unneccesary logs from sendmail file

--- cmd/web/sendMail.go
+++ cmd/web/sendMail.go
@@ -39,12 +39,11 @@ func sendMessage(m models.MailData) {
 	cfg.AddDefaultHeader("partner-key", SENDINBLUE_API_KEY)
 
 	sib := sendinblue.NewAPIClient(cfg)
-	result, resp, err := sib.AccountApi.GetAccount(ctx)
+	_, _, err = sib.AccountApi.GetAccount(ctx)
 	if err != nil {
 		fmt.Println("Error when calling AccountApi->get_account: ", err.Error())
 		return
 	}
-	fmt.Println("GetAccount Object:", result, " GetAccount Response: ", resp)
 
 	var emailContent string
 
